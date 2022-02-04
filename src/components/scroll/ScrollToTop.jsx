4fa91a7ce6b3031e4397572f80ad2c363df993dc
Lameck1 <lameck72@gmail.com>
2022-02-04 21:26:30 +0300

    Implement scroll to top funtionality

---
+++ src/components/scroll/ScrollToTop.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FaChevronUp } from 'react-icons/fa';
+
+const ScrollToTop = () => {
+  const [visible, setVisible] = useState(false);
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  });
+
+  const scrollUp = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <Div onClick={scrollUp}>
+      <div className={`scroll-to-top ${visible && 'active'}`}>
+        <FaChevronUp />
+      </div>
+    </Div>
+  );
+};
+
+const Div = styled.div`
+  .scroll-to-top {
+    z-index: 999;
+    position: fixed;
+    bottom: 20px;
+    right: 0;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    opacity: 0;
+    pointer-events: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 3.5rem;
+    height: 3.5rem;
+    border-radius: 50%;
+    background-color: var(--secondary-color);
+  }
+
+  .scroll-to-top.active {
+    opacity: 1;
+    pointer-events: auto;
+    right: 20px;
+  }
+
+  svg {
+    font-size: 1.5rem;
+    color: #fff;
+  }
+`;
+export default ScrollToTop;
