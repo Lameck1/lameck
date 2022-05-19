b563e58761693bc147645201c6cb19a79247eabf
Lameck1 <lameck72@gmail.com>
2022-05-19 07:22:53 +0300

    Add About component

---
+++ src/components/about/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { aboutAnimation } from '../../animation';
+
+const About = () => {
+  return (
+    <Section id="about">
+      <motion.div
+        className="about"
+        variants={aboutAnimation}
+        transition={{ delay: 0.3, duration: 0.6, type: 'tween' }}
+      >
+        <p>This is my about section.</p>
+      </motion.div>
+    </Section>
+  );
+};
+
+const Section = styled.section`
+  background-color: #f5f5f5;
+  min-height: 100vh;
+  padding: 5rem 0.5rem;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 992px) {
+  }
+`;
+
+export default About;
