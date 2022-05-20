6bd92ac7e4eb3e4d93586813a6189cb6d595e8c0
Lameck1 <lameck72@gmail.com>
2022-05-20 18:08:58 +0300

    Add Splash screen, Refactor App component

--- src/App.js
+++ src/App.js
@@ -8,17 +8,30 @@ import Navbar from './components/navbar/Navbar';
 import Projects from './components/projects/Projects';
 import ScrollToTop from './components/scroll/ScrollToTop';
 import { motion } from 'framer-motion';
+import Splash from './components/splash/splash';
 
 const App = () => {
+  const [showSplash, setShowSplash] = React.useState(true);
+  const [showPage, setShowPage] = React.useState(false);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+      setShowPage(true);
+    }, 3500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <motion.div initial="hide" animate="show">
-      <Navbar />
-      <ScrollToTop />
-      <Home />
-      <Projects />
-      <About />
-      <Contact />
-      <Footer />
+      {showSplash && <Splash />}
+      {showPage && <Navbar />}
+      {showPage && <ScrollToTop />}
+      {showPage && <Home />}
+      {showPage && <Projects />}
+      {showPage && <About />}
+      {showPage && <Contact />}
+      {showPage && <Footer />}
     </motion.div>
   );
 };
