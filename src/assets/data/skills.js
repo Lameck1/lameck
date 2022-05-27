de472457c9e4d31e52f5cc1c94b7002552b24997
Lameck1 <lameck72@gmail.com>
2022-05-27 23:03:43 +0300

    Add skills data

---
+++ src/assets/data/skills.js
@@ -0,0 +1,77 @@
+const skills = [
+  {
+    id: 0,
+    name: 'Languages',
+    items: [
+      {
+        id: 0,
+        name: 'HTML5',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/html-2752158-2284975.png',
+      },
+      {
+        id: 1,
+        name: 'CSS3',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/css-131-722685.png',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/javascript-1-225993.png',
+      },
+      {
+        id: 3,
+        name: 'Sass',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/sass-226054.png',
+      },
+      {
+        id: 4,
+        name: 'Ruby',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/ruby-226055.png',
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: 'Frameworks',
+    items: [
+      {
+        id: 0,
+        name: 'React',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/react-3-1175109.png',
+      },
+      {
+        id: 1,
+        name: 'Redux',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/redux-283024.png',
+      },
+      {
+        id: 2,
+        name: 'Rails',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/rails-2-1175112.png',
+      },
+      {
+        id: 3,
+        name: 'Bootstrap',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/bootstrap-226077.png',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Skills',
+    items: [
+      {
+        id: 0,
+        name: 'Remote Work',
+        icon: 'https://cdn.iconscout.com/icon/premium/png-64-thumb/remote-work-3131853-2638806.png',
+      },
+      {
+        id: 1,
+        name: 'Collaboration',
+        icon: 'https://cdn.iconscout.com/icon/free/png-64/collaboration-1538841-1304462.png',
+      },
+    ],
+  },
+];
+
+export default skills;
