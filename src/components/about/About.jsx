b2a24b7d30dfc466c98ba93e1753b5675964c02f
Lameck1 <lameck72@gmail.com>
2022-05-27 23:04:30 +0300

    Import Accordion component and style About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,31 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { aboutAnimation } from '../../animation';
+import { useState } from 'react';
+import Accordion from './Accordion';
+import skills from '../../assets/data/skills';
 
 const About = () => {
+  const [expanded, setExpanded] = useState(0);
+
   return (
     <Section id="about">
-      <motion.div
-        className="about"
-        variants={aboutAnimation}
-        transition={{ delay: 0.3, duration: 0.6, type: 'tween' }}
-      >
-        <p>This is my about section.</p>
-      </motion.div>
+      <h2>About Me</h2>
+      <div className="accordion">
+        {skills.map((skill) => (
+          <Accordion
+            key={skill.id}
+            i={skill.id}
+            expanded={expanded}
+            setExpanded={setExpanded}
+            header={skill.name}
+            items={skill.items}
+          />
+        ))}
+      </div>
     </Section>
   );
 };
 
 const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
   background-color: inherit;
   min-height: 100vh;
   padding: 5rem 1rem;
+  font-family: var(--font-sans);
+
+  h2 {
+    font-size: 2.5rem;
+    padding-bottom: 1rem;
+    color: black;
+    -webkit-text-fill-color: transparent;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: var(--lightest-slate);
+    letter-spacing: 0.4rem;
+  }
+
+  .accordion {
+    width: 95vw;
+  }
 
   @media screen and (min-width: 768px) {
+    .accordion {
+      width: 80vw;
+    }
   }
 
   @media screen and (min-width: 992px) {
+    .accordion {
+      width: 40vw;
+    }
   }
 `;
 
