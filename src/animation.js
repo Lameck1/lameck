67872b174cba09bfa69d5c4bafa7eb1625e143e4
Lameck1 <lameck72@gmail.com>
2022-05-27 23:02:10 +0300

    Remove obsolete animation rules

--- src/animation.js
+++ src/animation.js
@@ -9,19 +9,9 @@ export const homeAnimation = {
   },
 };
 
-// export const homeInfoAnimation = {
-//   hidden: { x: 100, opacity: 0 },
-//   show: { x: 0, opacity: 1 },
-// };
-
 export const aboutAnimation = {
   hide: { y: 0, opacity: 0 },
   show: { y: 1, opacity: 1 },
-};
-
-export const projectsAnimation = {
-  hide: { scale: 0.5, opacity: 0 },
-  show: { scale: 1, opacity: 1 },
 };
 
 export const contactAnimation = {
