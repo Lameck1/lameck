c564a4a0285d52f553d2611920024af47fceadd2
Lameck1 <lameck72@gmail.com>
2022-05-27 23:05:25 +0300

    Restyle Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,6 +6,7 @@ import { contactAnimation } from '../../animation';
 const Contact = () => {
   return (
     <Section id="contact">
+      <h2>Get in touch</h2>
       <motion.div
         className="contact"
         variants={contactAnimation}
@@ -18,9 +19,23 @@ const Contact = () => {
 };
 
 const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: inherit;
   min-height: 100vh;
   padding: 5rem 1rem;
+  font-family: var(--font-sans);
+
+  h2 {
+    font-size: 2.5rem;
+    padding-bottom: 1rem;
+    color: black;
+    -webkit-text-fill-color: transparent;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: var(--lightest-slate);
+    letter-spacing: 0.4rem;
+  }
 
   @media screen and (min-width: 768px) {
   }
