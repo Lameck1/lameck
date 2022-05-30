50f37ceb9b608be0f54da132b008ca4dc838da09
Lameck1 <lameck72@gmail.com>
2022-05-30 17:07:42 +0300

    Add about text and style About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,6 +10,28 @@ const About = () => {
   return (
     <Section id="about">
       <h2>About Me</h2>
+      <div className="text">
+        <p>
+          Hi, I'm Lameck Otieno and I love building solutions for the web. My
+          passion for web development got built from curiosity on how the web
+          works and how it can be used to solve problems.
+        </p>
+        <p>
+          I love learning, and software development fulfills that need. It's
+          always everchanging, and I'm always looking to learn more. Nothing
+          would make me happy than being a member of an engineering team focused
+          on developing web technology solutions for businesses and the greater
+          good, and feed my enthusiasm for converting business ideas into
+          attractive web applications.
+        </p>
+        <p>
+          While learning software development, I have honed the relevant
+          technical skills and have been able to apply them in a variety of
+          projects. The accordion below summarizes some of the skills I have
+          acquired.
+        </p>
+      </div>
+
       <div className="accordion">
         {skills.map((skill) => (
           <Accordion
@@ -36,14 +58,12 @@ const Section = styled.section`
   padding: 5rem 1rem;
   font-family: var(--font-sans);
 
-  h2 {
-    font-size: 2.5rem;
-    padding-bottom: 1rem;
-    color: black;
-    -webkit-text-fill-color: transparent;
-    -webkit-text-stroke-width: 1px;
-    -webkit-text-stroke-color: var(--lightest-slate);
-    letter-spacing: 0.4rem;
+  p {
+    font-size: clamp(1rem, 2vw, 1.2rem);
+    color: var(--lightest-slate);
+    margin-bottom: 0.5rem;
+    text-align: justify;
+    text-justify: inter-word;
   }
 
   .accordion {
@@ -51,12 +71,18 @@ const Section = styled.section`
   }
 
   @media screen and (min-width: 768px) {
+    .text {
+      width: 60vw;
+    }
     .accordion {
-      width: 80vw;
+      width: 50vw;
     }
   }
 
   @media screen and (min-width: 992px) {
+    .text {
+      width: 50vw;
+    }
     .accordion {
       width: 40vw;
     }
